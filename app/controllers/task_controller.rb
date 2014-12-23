0ce32bc565353d85fc153e073fa45b1df8c2e115
puncha <punchapuncha@gmail.com>
2014-12-23 16:09:09 +0800

    Implement List all/completed/uncompleted tasks

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -3,6 +3,12 @@ class TaskController < ApplicationController
   end
 
   def list
+    filter = params[:filter] || 'all'
+    case filter
+      when 'all' then @tasks = Task.all
+      when 'completed' then @tasks = Task.where completed: true
+      when 'uncompleted' then @tasks = Task.where completed: false
+    end
   end
 
   def show
