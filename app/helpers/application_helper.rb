9daa1473505093e318976274ec6396e95ad6ff5b
puncha <punchapuncha@gmail.com>
2014-12-26 23:31:22 +0800

    Use helpers to render flash notice and errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def bootstrap_flash_notice
+    unless flash[:notice].nil?
+      render :partial=>'helper_fragments/flash_notice', :locals=>{notice:flash[:notice]}
+    end
+  end
+
+  def bootstrap_flash_errors
+    errors = flash[:errors]
+    unless errors.nil? or errors.empty?
+      render :partial=>'helper_fragments/flash_errors', :locals=>{errors:errors}
+    end
+  end
 end
